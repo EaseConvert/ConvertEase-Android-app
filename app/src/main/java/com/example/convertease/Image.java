d3430bfa91b00e26e4b2736521ca34756f35439b
wakeupssid <siddhantpillewar@gmail.com>
2023-10-10 13:29:50 +0530

    committed

---
+++ app/src/main/java/com/example/convertease/Image.java
@@ -0,0 +1,14 @@
+package com.example.convertease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Image extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}
